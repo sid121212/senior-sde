e094fe3cc1d3f50735178beb7a0570494c6d659b
sid121212 <siddharthmehta121212@gmail.com>
2026-06-21 21:44:01 +0530

    Added company logos

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,9 @@
-import Link from 'next/link'
+import CompanyMarquee from './CompanyMarquee'
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link href="/dashboard" className="navbar__logo">
-        Prep<span>OS</span>
-      </Link>
+      <CompanyMarquee />
 
       <div className="navbar__actions">
         <span className="caption">v0.1</span>
